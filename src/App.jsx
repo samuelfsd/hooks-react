febfac1a706db3bc7e8eedae6a64e6216d9fda51
samuelfsd <samuel.estudo778@gmail.com>
2023-05-09 21:45:50 -0300

    add fake page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import { PageEstado } from './pages/PageEstado'
 
 
 // useState()
@@ -7,12 +6,46 @@ import { PageEstado } from './pages/PageEstado'
 // useContext()
 // useMemo()
 // useCallback()
+const Header = () => {
+  return (
+    <header><h1>hooks header</h1></header>
+  )
+}
+
+const Menu = () => {
+  return (
+    <nav>Menu</nav>
+  )
+}
+
+const Sidebar = () => {
+  return (
+    <aside>Sidebar</aside>
+  )
+}
+
+const Content = () => {
+  return (
+    <main>Content</main>
+  )
+}
+
+const Footer = () => {
+  return (
+    <footer>footer</footer>
+  )
+}
+
 
 function App() {
 
   return (
     <>
-     <PageEstado />
+      <Header />
+      <Menu />
+      <Sidebar />
+      <Content />
+      <Footer />  
     </>
   )
 }
